fb0fd6cc921d6338c1a1a7393eb631c5829053f1
Agustin800 <aguslahalo@gmail.com>
2025-04-18 20:38:06 +0000

    Cambios en shell.c, adaptado para no permite argumentos (solo para el ejercicio 2 cuya letra lo especifica)

--- shell.c
+++ shell.c
@@ -9,10 +9,10 @@
 
 int main(void)
 {
-	char *line = NULL, **args, *token;
+	char *line = NULL;
+	char *args[] = {NULL};
 	size_t len = 0;
 	ssize_t nread;
-	int i = 0;
 
 	while (1)
 	{
@@ -20,26 +20,14 @@ int main(void)
 		nread = getline(&line, &len, stdin);
 		if (nread == -1)
 			break;
+
 		line[strcspn(line, "\n")] = 0;
 
-
-		args = malloc(10 * sizeof(char *));
-		if (!args)
-			exit(EXIT_FAILURE);
-
-		i = 0;
-		token = strtok(line, " ");
-		while (token && i < 9)
+		if (line[0] != '\0')
 		{
-			args[i++] = token;
-			token = strtok(NULL, " ");
+			args[0] = line;
+			execute_command(line, args);
 		}
-		args[i] = NULL;
-
-		if (args[0])
-			execute_command(args[0], args);
-
-		free(args);
 	}
 	free(line);
 	return (0);
