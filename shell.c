7836dd647640497d59f40355016f7fc22463a73b
gastii11 <10632@holbertonstudents.com>
2025-04-19 00:19:31 +0000

    prueba 3

--- shell.c
+++ shell.c
@@ -2,49 +2,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-/**
- * main - simple shell
- * Return: retorna 0
- */
-
-int main(void)
-{
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t nread;
-	pid_t pid;
-
-	while (1)
-	{
-		printf("$ ");
-
-		nread = getline(&line, &len, stdin);
-		if (nread == -1)
-		{
-			printf("\n");
-			break;
-		}
-
-		line[strcspn(line, "\n")] = 0;
-
-		pid = fork();
-		if (pid == 0)
-		{
-			if (execve(line, &line, NULL) == -1)
-			{
-				perror("Error:");
-				exit(EXIT_FAILURE);
-			}
-		}
-		else if (pid < 0)
-		{
-			perror("fork failed");
-		}
-		else
-		{
-			wait(NULL);
-		}
-	}
-	free(line);
-	return (0);
-}
+ /**
+  * main - simple shell
+  * Return: retorna 0
+  */
+ 
+ int main(void)
+ {
+ 	char *line = NULL, **args, *token;
+ 	size_t len = 0;
+ 	ssize_t nread;
+ 	int i = 0;
+ 
+ 	while (1)
+ 	{
+ 		printf("$ ");
+ 		nread = getline(&line, &len, stdin);
+ 		if (nread == -1)
+ 			break;
+ 		line[strcspn(line, "\n")] = 0;
+ 
+ 
+ 		args = malloc(10 * sizeof(char *));
+ 		if (!args)
+ 			exit(EXIT_FAILURE);
+ 
+ 		i = 0;
+ 		token = strtok(line, " ");
+ 		while (token && i < 9)
+ 		{
+ 			args[i++] = token;
+ 			token = strtok(NULL, " ");
+ 		}
+ 		args[i] = NULL;
+ 
+ 		if (args[0])
+ 			execute_command(args[0], args);
+ 
+ 		free(args);
+ 	}
+ 	free(line);
+ 	return (0);
+ }
