2269cc1aaa781bf0234a6f0eba5887c5d40a97b9
gastii11 <10632@holbertonstudents.com>
2025-04-21 14:58:45 +0000

    arreglamos erorres de betty

--- exec.c
+++ exec.c
@@ -11,45 +11,42 @@
  */
 void execute_command(char *command, char *args[])
 {
-    pid_t pid;
-    int status;
-    char *path, *path_copy, *token, full_path[256];
+	pid_t pid;
+	int status;
+	char *path, *path_copy, *token, full_path[256];
 
-    if (strchr(command, '/'))
-    {
-        execve(command, args, environ);
-        perror("./simple_shell");
-        exit(EXIT_FAILURE);
-    }
+	if (strchr(command, '/'))
+	{
+		execve(command, args, environ);
+		perror("./simple_shell");
+		exit(EXIT_FAILURE);
+	}
+	path = getenv("PATH");
+	if (!path)
+		return;
 
-    path = getenv("PATH");
-    if (!path)
-        return;
+	path_copy = strdup(path);
+	token = strtok(path_copy, ":");
+	while (token)
+	{
+		snprintf(full_path, sizeof(full_path), "%s/%s", token, command);
+		if (access(full_path, X_OK) == 0)
+		{
+			pid = fork();
+			if (pid == 0)
+			{
+				execve(full_path, args, environ);
+				perror("./simple_shell");
+				exit(EXIT_FAILURE);
+			}
+			else if (pid > 0)
+				wait(&status);
 
-    path_copy = strdup(path);
-    token = strtok(path_copy, ":");
-
-    while (token)
-    {
-        snprintf(full_path, sizeof(full_path), "%s/%s", token, command);
-        if (access(full_path, X_OK) == 0)
-        {
-            pid = fork();
-            if (pid == 0)
-            {
-                execve(full_path, args, environ);
-                perror("./simple_shell");
-                exit(EXIT_FAILURE);
-            }
-            else if (pid > 0)
-                wait(&status);
-
-            free(path_copy);
-            return;
-        }
-        token = strtok(NULL, ":");
-    }
-
-    free(path_copy);
-    fprintf(stderr, "%s: comando no encontrado\n", command);
+			free(path_copy);
+			return;
+		}
+		token = strtok(NULL, ":");
+	}
+	free(path_copy);
+	fprintf(stderr, "%s: comando no encontrado\n", command);
 }
--- shell.c
+++ shell.c
@@ -2,45 +2,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
- /**
-  * main - simple shell
-  * Return: retorna 0
-  */
- 
- int main(void)
- {
- 	char *line = NULL, **args, *token;
- 	size_t len = 0;
- 	ssize_t nread;
- 	int i = 0;
- 
- 	while (1)
- 	{
- 		printf("$ ");
- 		nread = getline(&line, &len, stdin);
- 		if (nread == -1)
- 			break;
- 		line[strcspn(line, "\n")] = 0;
- 
- 
- 		args = malloc(10 * sizeof(char *));
- 		if (!args)
- 			exit(EXIT_FAILURE);
- 
- 		i = 0;
- 		token = strtok(line, " ");
- 		while (token && i < 9)
- 		{
- 			args[i++] = token;
- 			token = strtok(NULL, " ");
- 		}
- 		args[i] = NULL;
- 
- 		if (args[0])
- 			execute_command(args[0], args);
- 
- 		free(args);
- 	}
- 	free(line);
- 	return (0);
- }
+/**
+ * main - simple shell
+ * Return: retorna 0
+ */
+int main(void)
+{
+	char *line = NULL, **args, *token;
+	size_t len = 0;
+	ssize_t nread;
+	int i = 0;
+
+
+	while (1)
+	{
+		printf("$ ");
+		nread = getline(&line, &len, stdin);
+		if (nread == -1)
+			break;
+		line[strcspn(line, "\n")] = 0;
+
+
+		args = malloc(10 * sizeof(char *));
+		if (!args)
+			exit(EXIT_FAILURE);
+
+		i = 0;
+		token = strtok(line, " ");
+		while (token && i < 9)
+		{
+			args[i++] = token;
+			token = strtok(NULL, " ");
+		}
+		args[i] = NULL;
+
+		if (args[0])
+			execute_command(args[0], args);
+
+		free(args);
+	}
+	free(line);
+	return (0);
+}
