8f9c813288a9e897c6571421ef2fa208d293aadd
Agustin800 <aguslahalo@gmail.com>
2025-04-24 10:03:24 -0300

    eliminacion de la variable envirion

--- main.h
+++ main.h
@@ -7,9 +7,6 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-
-extern char **environ;
-
 void execute_command(char *command, char *args[]);
 void print_prompt(void);
 void clean_newline(char *str);
